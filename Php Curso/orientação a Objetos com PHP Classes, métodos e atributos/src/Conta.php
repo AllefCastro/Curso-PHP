57e7d4aee54909aca0f1e05d64246071466eda3c
AllefCastroPimentel <67397546+AllefCastroPimentel@users.noreply.github.com>
2024-02-09 11:27:19 -0300

    Add files via upload

--- Php Curso/orientação a Objetos com PHP Classes, métodos e atributos/src/Conta.php
+++ Php Curso/orientação a Objetos com PHP Classes, métodos e atributos/src/Conta.php
@@ -6,28 +6,26 @@ class Conta
     public string $cpfTitular;
     public string $nomeTitular;
     public float $saldo;
+
+    public function sacar(float $valorASacar)
+    {
+        if($valorASacar > $this->saldo){
+            echo "Saldo indisponivel";
+        } else{
+            $this->saldo -= $valorASacar;
+        }
+    }
+    public function depositar(float $valorADepositar)
+    {
+        if($valorADepositar <=0){
+            echo "Valor precisa ser maior que 0";
+        } else{
+            $this->saldo += $valorADepositar;
+        }
+    }
+
+
 }
 
 
 
-$primeiraConta = new Conta();
-$primeiraConta->saldo = 200;
-$primeiraConta->cpfTitular = '123.456.789-10';
-$primeiraConta->nomeTitular = 'Vinicius Dias';
-var_dump($primeiraConta); // Verifique que o objeto já possui os dados necessários de uma conta criada
-$segundaConta = new Conta();
-$segundaConta->cpfTitular = '987.654.321-10';
-$segundaConta->nomeTitular = 'Patricia';
-$segundaConta->saldo = 1500;
-var_dump($primeiraConta); // Verifique que o objeto não teve seus valores alterados
-var_dump($segundaConta); // Note que este objeto possui os valores da segunda conta criada
-
-$a = 4;
-$b = $a;
-$b++;
-echo $b;
-echo $a;
-
-$c = $segundaConta;
-$c->saldo = 2000;
-var_dump($segundaConta);
